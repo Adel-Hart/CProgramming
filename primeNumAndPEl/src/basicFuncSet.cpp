6f0fd7c96810b4d4829d39dfb34c5748cefd8146
Adel-Hart <donghoon777.kim@gmail.com>
2025-03-19 15:38:22 +0900

    combine to main.cpp
    1. comining all files to main.cpp
    for release to baekjoon

--- primeNumAndPEl/src/basicFuncSet.cpp
+++ primeNumAndPEl/src/basicFuncSet.cpp
@@ -1,34 +1,34 @@
-#include "basicFuncSet.h"
+// #include "basicFuncSet.h"
 
-#include <iostream>
+// #include <iostream>
 
-int getDigit(int n) {
-    int count = 0;
-    while (n != 0) {
-        n /= 10;
-        count++;
-    }
-    return count;
-}
+// int getDigit(int n) {
+//     int count = 0;
+//     while (n != 0) {
+//         n /= 10;
+//         count++;
+//     }
+//     return count;
+// }
 
-bool isOdd(int n){
-    if (n % 2 == 0) { //2로 나뉘어지면 짝수
-        return false;
-    }
-    return true;
-}
+// bool isOdd(int n){
+//     if (n % 2 == 0) { //2로 나뉘어지면 짝수
+//         return false;
+//     }
+//     return true;
+// }
 
-int* digitSeparate(int n){
+// int* digitSeparate(int n){
 
-    int digit = getDigit(n);
-    int* arr = new int[digit]; //heap에 배열 할당 
+//     int digit = getDigit(n);
+//     int* arr = new int[digit]; //heap에 배열 할당 
     
-    for(int i = 0; i < digit; i++){
-        arr[digit - i - 1] = n % 10; //배열에 inverse저장이 되서 digit - i - 1로 123 -> 1, 2, 3 저장.
-        n /= 10; //각 자리수 배열에 저장
+//     for(int i = 0; i < digit; i++){
+//         arr[digit - i - 1] = n % 10; //배열에 inverse저장이 되서 digit - i - 1로 123 -> 1, 2, 3 저장.
+//         n /= 10; //각 자리수 배열에 저장
         
-    }
+//     }
 
-    return arr; //주소 리턴
-}
+//     return arr; //주소 리턴
+// }
  
--- primeNumAndPEl/src/main.cpp
+++ primeNumAndPEl/src/main.cpp
@@ -3,11 +3,98 @@
 #include <iostream>
 #include <time.h>
 
-#include "basicFuncSet.h"
-#include "funcSet.h"
-
 
 using namespace std;
+
+
+int getDigit(int n) {
+    int count = 0;
+    while (n != 0) {
+        n /= 10;    
+        count++;
+    }
+    return count;
+}
+
+bool isOdd(int n){
+    if (n % 2 == 0) { //2로 나뉘어지면 짝수
+        return false;
+    }
+    return true;
+}
+
+int* digitSeparate(int n){
+
+    int digit = getDigit(n);
+    int* arr = new int[digit]; //heap에 배열 할당 
+    
+    for(int i = 0; i < digit; i++){
+        arr[digit - i - 1] = n % 10; //배열에 inverse저장이 되서 digit - i - 1로 123 -> 1, 2, 3 저장.
+        n /= 10; //각 자리수 배열에 저장
+        
+    }
+
+    return arr; //주소 리턴
+}
+ 
+
+bool isPalin(int num, int digit){ //팰린드롬s 판별 함수.
+    
+    int cycle = digit / 2; //자릿수의 반만큼만 비교하면 됨.
+
+    int* digitArry = digitSeparate(num); //자릿수 별로 배열에 저장. 사용 후 delete 필요 
+
+
+    if(isOdd(digit)){ //홀수 자리면? xxxyxxx 꼴 , cycle은 y전 x임!
+
+            
+        for (int i = 0; i < cycle; i++) {
+            if(digitArry[cycle -1 - i] != digitArry[cycle + 1 + i]){
+                delete digitArry; //사용 후 메모리 해제
+                return false;
+            }
+            /*
+            안쪽에서 부터 비교, 전꺼 - 1 : 인덱스 0부터 시작이라서,
+            나중꺼 +1 : 홀수 자릿수라 가운데 꺼 무시 + 2에 인덱스 0부터 시작으로 -1 총 +!
+            
+            저 if 문이 한번이라도 false가 나오면 팰린드롬이 아님.
+            그래서 return으로 for 탈출
+            */
+        }
+        
+
+        delete digitArry; //사용 후 메모리 해제
+
+        return true;
+
+    }
+
+    else{ //짝수이면
+        
+        for (int i = 0; i < cycle; i++) {
+            if(digitArry[cycle - 1 - i] != digitArry[cycle + i]){
+                delete digitArry; //사용 후 메모리 해제
+                return false;
+            } //홀수는 가운데에 숫자가 없어서, 위 로직에서 뒤에꺼 +1이 없음.
+    
+        }
+        delete digitArry; //사용 후 메모리 해제
+        return true; 
+    }
+}
+
+bool isPrime(int num){
+    for(int i = 2; i < sqrt(num); i++){
+
+        if(num % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
 
 int main(){
 
